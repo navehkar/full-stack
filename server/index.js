42c54fd9cf6a1f3f5b4f93854e7e637b27a099ab
navehkar <navehkarni13@gmail.com>
2025-02-27 18:13:37 +0200

    cors

--- server/index.js
+++ server/index.js
@@ -15,7 +15,8 @@ app.use(cors({
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
-}))
+  exposedHeaders: ['set-cookie'],
+}));
 app.use('/api',routes);
 
 app.listen(3000, () => {
--- server/lib/utils.js
+++ server/lib/utils.js
@@ -1,18 +1,24 @@
 const jwt = require('jsonwebtoken');
-function setTokenCookie(res, user, secret){
+function setTokenCookie(res, user, secret) {
     const token = jwt.sign(
         {
             id: user._id,
             username: user.username,
+            fullName: user.fullName,
+            email: user.email
         },
         secret,
-        {expiresIn:'24h'}
+        { expiresIn: '24h' }
     );
+    
     res.cookie('token', token, {
         httpOnly: true,
-        sameSite: 'strict',
+        secure: process.env.NODE_ENV === 'production', // true in production
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax', // important for cross-site
         maxAge: 24 * 60 * 60 * 1000, // 24 hours
-      });
+        path: '/',
+        domain: process.env.NODE_ENV === 'production' ? '.full-stack-z8dl.onrender.com' : 'localhost'
+    });
 }
 module.exports ={
     setTokenCookie,
