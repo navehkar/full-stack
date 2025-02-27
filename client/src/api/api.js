f8668cdc8546ac60aa9793f94d721b2660410200
navehkar <navehkarni13@gmail.com>
2025-02-27 10:43:33 +0200

    api.js

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
-
+const baseURL = process.env.NODE_ENV === 'production' ? 'https://full-stack-z8dl.onrender.com/api' : 'http://localhost:3000/api'
 export default axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL,
     headers:{
         'Content-type':'application/json',
     },
