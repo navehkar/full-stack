f87d86cc02cd57e32774758527add8d61f5aa765
navehkar <navehkarni13@gmail.com>
2025-03-11 11:51:18 +0200

    deploy4

--- server/index.js
+++ server/index.js
@@ -14,9 +14,6 @@ app.use(cookieParser());
 app.use(cors({
   origin: ['https://full-stack-6sdv.onrender.com', 'http://localhost:5173'],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
-  exposedHeaders: ['Set-Cookie'],
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api',routes);
