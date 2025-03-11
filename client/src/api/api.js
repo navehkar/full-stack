6db692272d94703844e73cbb6570484effd19c35
navehkar <navehkarni13@gmail.com>
2025-03-11 11:30:44 +0200

    deploy

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const baseURL = process.env.NODE_ENV === 'production' 
-  ? 'https://full-stack-z8dl.onrender.com/api' 
+  ? '/api' 
   : 'http://localhost:3000/api';
 
 export default axios.create({
