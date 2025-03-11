b84a0d2addaef590e9f4585f216893f7ce999e18
navehkar <navehkarni13@gmail.com>
2025-03-11 11:47:52 +0200

    deploy3

--- server/index.js
+++ server/index.js
@@ -7,17 +7,20 @@ const connectDB = require('./lib/connect')
 const bcrypt = require('bcrypt')
 const cookieParser = require('cookie-parser')
 const cors = require('cors');
+const path = require('path');
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ['https://oy-vay-pay-naveh1234.netlify.app', 'http://localhost:5173'],
+  origin: ['https://full-stack-6sdv.onrender.com', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Cookie'],
   exposedHeaders: ['Set-Cookie'],
 }));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api',routes);
+app.get('*', (req, res) => { res.sendFile(path.join(__dirname, 'public', 'index.html')); });
 
 app.listen(3000, () => {
   connectDB();
